6e006adcdf18ecd9281a1c7be9c3092571d28e64
Scott <scott.ti312@gmail.com>
2023-05-29 19:54:28 -0700

    Add request test and chatengine users api

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import { creds } from "./chatenginecreds.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put('https://api.chatengine.io/users/',
+    { username: username, secret: username, first_name: username },
+    { headers: { "private-key": creds } });
+    return res.status(r.status).json(r.data);
+  }
+  catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+app.listen(3001);
