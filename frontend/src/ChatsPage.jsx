99dc86d3553f7963996e4afc4d240012cb66a36a
Scott <scott.ti312@gmail.com>
2023-05-29 20:33:20 -0700

    Add chats page with react-chat-engine-pretty

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-  return <>chats...</>
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+import { creds } from "./chatenginecreds.js";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: '100vh' }}>
+      <PrettyChatWindow
+        projectID={creds}
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: '100%' }}
+        />
+    </div>
+  )
 }
 
 export default ChatsPage;
