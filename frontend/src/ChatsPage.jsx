9b572bffedfa99bdafb47f0028c8e8b044738ca8
Scott <scott.ti312@gmail.com>
2023-05-29 20:45:45 -0700

    Trying react-chat-engine-advanced

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,17 +1,12 @@
-import { PrettyChatWindow } from "react-chat-engine-pretty";
-import { creds } from "./chatenginecreds.js";
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced";
+import { creds} from "./chatenginecreds.js";
 
 const ChatsPage = (props) => {
-  return (
-    <div style={{ height: '100vh' }}>
-      <PrettyChatWindow
-        projectID={creds}
-        username={props.user.username}
-        secret={props.user.secret}
-        style={{ height: '100%' }}
-        />
-    </div>
-  )
+  const chatProps = useMultiChatLogic(creds, props.user.username, props.user.secret)
+  return <div style={{height: '100vh' }}>
+    <MultiChatSocket {...chatProps} />
+    <MultiChatWindow {...chatProps} style={{ height: '100%' }}/>
+  </div>
 }
 
 export default ChatsPage;
